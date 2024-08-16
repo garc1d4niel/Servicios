47c278d28cc29609d90c6323a5c3507663e0823c
daniel garcia <garciaacevedodaniel@gmail.com>
2024-08-15 20:59:40 -0500

    Proyecto uno SOAP

---
+++ ProtectoUnoSoap/ProtectoUnoSoap/src/main/java/com/co/garcia/ProtectoUnoSoap/ProtectoUnoSoapApplication.java
@@ -0,0 +1,13 @@
+package com.co.garcia.ProtectoUnoSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectoUnoSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectoUnoSoapApplication.class, args);
+	}
+
+}
---
+++ ProtectoUnoSoap/ProtectoUnoSoap/src/test/java/com/co/garcia/ProtectoUnoSoap/ProtectoUnoSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.garcia.ProtectoUnoSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtectoUnoSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
