30d84e1ac0ce6d12a3d492558e23429edf0b1fb1
Daniel Garcia Acevedo <garciaacevedodaniel066@gmail.com>
2024-08-29 20:57:27 -0500

    Verbos HTTP

---
+++ Daniel/src/main/java/com/co/Garcia/Daniel/DanielApplication.java
@@ -0,0 +1,13 @@
+package com.co.Garcia.Daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielApplication.class, args);
+	}
+
+}
